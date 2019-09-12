fba57df6b196bdc8063f84d27d3e015b09fe6ce9
Tino Fuhrmann <tino-fuhrmann@web.de>
2019-09-12 15:11:58 -0400

    Fixed equals

--- dyndns.sh
+++ dyndns.sh
@@ -56,7 +56,7 @@ if [ -n "$current" ]; then
   fi
 fi
 if [ -n "$ipv4" ]; then
-  if [ "$oldv4" = "$ipv4" ]; then 
+  if [ "$oldv4" != "$ipv4" ]; then 
   	echo "Updating v4 to $ipv4"
   	echo "http://$subdomain:$password@dnshome.de/dyndns.php?ip=$ipv4"
   	$bin "http://$subdomain:$password@dnshome.de/dyndns.php?ip=$ipv4"
